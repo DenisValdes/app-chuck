ae07baad7a831e74c8903bf7ff567221145293a8
DennisVAldes <dennis.valdez@anima.edu.uy>
2019-08-29 09:28:49 -0300

    Este es un commit

--- tarea_git_sistemas.bash
+++ tarea_git_sistemas.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Agregar archivos al commit
+#Agregar archivos al commit
 
 git add .
 
-# Hacer commit con mensaje al repo
+#Hacer commit con mensaje al repo
 
 git commit -m "Este es un commit "
 
-# Realizar push
+#Realizar push
 
 git push origin master
 
-# Mostrar estatus del repo
+#Mostrar estatus del repo
 
 git status
